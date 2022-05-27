d1b4cda24066febbaeb08c6c3182309ecc667b7d
Kate-asiel <93286880+Kate-asiel@users.noreply.github.com>
2022-05-27 15:52:50 +0300

    added lab9

---
+++ pythonllab1/publications.py
@@ -0,0 +1,41 @@
+class Publication:
+
+    def __init__(self, name: str, price: float):
+        self.name = name
+        self.price = price
+
+    def __str__(self):
+        return f"Name: {self.name}, Price: {self.price}"
+
+
+class Book(Publication):
+    def __init__(self, name: str, price: float, author: str, book_type: str):
+        super().__init__(name, price)
+        self.author = author
+        self.book_type = book_type
+
+    def __str__(self):
+        return f"Book - " + super(Book, self).__str__() \
+               + f", Author: {self.author}, Book type: {self.book_type}"
+
+
+class Monograph(Publication):
+    def __init__(self, name: str, price: float, topic: str, count_of_pages: int):
+        super().__init__(name, price)
+        self.count_of_pages = count_of_pages
+        self.topic = topic
+
+    def __str__(self):
+        return f"Monograph - " + super(Monograph, self).__str__() \
+               + f", Count of pages: {self.count_of_pages}, Topic: {self.topic}"
+
+
+class Magazine(Publication):
+    def __init__(self, name: str, price: float, publisher: str, circulation: int):
+        super().__init__(name, price)
+        self.publisher = publisher
+        self.circulation = circulation
+
+    def __str__(self):
+        return f"Magazine - " + super(Magazine, self).__str__() \
+               + f", Publisher: {self.publisher}, Circulation: {self.circulation}"
